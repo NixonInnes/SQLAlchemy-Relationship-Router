4a8d03bacbfe3642e4034a9ae0e8e3e2e12361f1
James NixonInnes <nixoninnes@gmail.com>
2016-03-25 14:22:56 +0000

    few tweaks about init

--- app/dijkstra.py
+++ app/dijkstra.py
@@ -1,25 +1,30 @@
 from sqlalchemy import inspect
 from collections import defaultdict
 from heapq import heappop, heappush
-from app.db import Base
+#from app.db import Base
 
 class Graph(object):
     def __init__(self, Base):
-        self.tables = self.get_tables(Base)
+        self.tables = {}
+        self.make_tables(Base)
         self.relationships = []
-        self.get_relationships()
+        self.make_relationships()
+        self.graph = defaultdict(list)
+        self.make_graph()
         
-    def get_tables(self, Base):
-        tables = {}
+    def make_tables(self, Base):
         for cls in Base._decl_class_registry.values():
             if hasattr(cls, '__tablename__'):
-                tables[cls.__tablename__] = cls
-        return tables
+                self.tables[cls.__tablename__] = cls
 
-    def get_relationships(self):
+    def make_relationships(self):
         for table in self.tables:
             for rel in inspect(self.tables[table]).relationships:
                 self.relationships.append((self.tables[table], self.tables[rel.table.name], 1))
+
+    def make_graph(self):
+        for frm, to, dist in self.relationships:
+            self.graph[frm].append( (dist, to) ) 
 
     class Q(object):
         def __init__(self, dist, node, path):
@@ -33,15 +38,9 @@ class Graph(object):
         def __lt__(self, other):
             return self.dist < other.dist
 
-    def walk(self, start, target):
-        graph = defaultdict(list)
-
-        for frm, to, dist in self.relationships:
-            graph[frm].append( (dist, to) )
-
+    def get_route(self, start, target):
         queue = [self.Q(0, start, [])]
         checked = set()
-
         while queue:
             q = heappop(queue)
             if q.node not in checked:
@@ -49,10 +48,14 @@ class Graph(object):
                 path = q.path + [q.node]
                 if q.node is target:
                     return q.dist, path
-                for dist_n, node_n in graph.get(q.node):
+                for dist_n, node_n in self.graph.get(q.node):
                     if node_n not in checked:
                         heappush(queue, self.Q(q.dist+dist_n, node_n, path))
         return float('inf')
 
+    # Makes life a little easier
+    def table(self, tablename):
+        return self.tables.get(tablename)
+
 #g = Graph(Base)
-#g.walk(g.tables['mid-mids'], g.tables['top-rights'])
+#g.get_route(g.tables['mid-mids'], g.tables['top-rights'])
